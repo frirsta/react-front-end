b76ccfd9496e68f73bb000dcc79e76d9f0bc940d
frirsta <frista-8@hotmail.com>
2023-04-08 07:38:33 +0000

    Add username form so user can change their username

---
+++ src/pages/accounts/UsernameForm.js
@@ -0,0 +1,76 @@
+import React, { useEffect, useState } from "react";
+import Alert from "react-bootstrap/Alert";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import { useHistory, useParams } from "react-router-dom";
+import {
+  useCurrentUser,
+  useSetCurrentUser,
+} from "../../context/CurrentUserContext";
+import { axiosRes } from "../../api/axiosDefaults";
+import styles from '../../styles/Accounts.module.css'
+
+const UsernameForm = () => {
+  const [username, setUsername] = useState("");
+  const [errors, setErrors] = useState({});
+
+  const history = useHistory();
+  const { id } = useParams();
+
+  const currentUser = useCurrentUser();
+  const setCurrentUser = useSetCurrentUser();
+
+  useEffect(() => {
+    if (currentUser?.profile_id?.toString() === id) {
+      setUsername(currentUser.username);
+    } else {
+      history.push("/");
+    }
+  }, [currentUser, history, id]);
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      await axiosRes.put("/dj-rest-auth/user/", {
+        username,
+      });
+      setCurrentUser((prevUser) => ({
+        ...prevUser,
+        username,
+      }));
+      history.goBack();
+    } catch (err) {
+      console.log(err);
+      setErrors(err.response?.data);
+    }
+  };
+
+  return (
+    <div>
+      <Form className={styles.Form} onSubmit={handleSubmit}>
+        <Form.Group>
+          <Form.Label>Change Username</Form.Label>
+          <Form.Control
+            placeholder="username"
+            type="text"
+            value={username}
+            onChange={(event) => setUsername(event.target.value)}
+          />
+        </Form.Group>
+        {errors?.username?.map((message, idx) => (
+          <Alert key={idx} variant="warning">
+            {message}
+          </Alert>
+        ))}
+        <Button onClick={() => history.goBack()}>
+          cancel
+        </Button>
+        <Button type="submit">
+          save
+        </Button>
+      </Form>
+    </div>
+  );
+};
+
+export default UsernameForm;
